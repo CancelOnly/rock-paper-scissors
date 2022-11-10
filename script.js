4d9afc6e47115726b4efb42c5a72c3cb7c4c108d
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2022-11-10 02:51:05 -0300

    testing functions

--- script.js
+++ script.js
@@ -34,10 +34,11 @@ function playRound(playerSelection, computerSelection) {
 function getPlayerChoice() {
     let validatedInput = false;
     while (validatedInput == false) {
-        const choice = prompt("Rock, Paper & Scissors", "Test your might!");
+        const choice = prompt("Battle with your life! Choose:", "Rock, Paper or Scissors");
         if (choice == null) {
             continue;
         } const choiceInLower = choice.toLocaleLowerCase();
+
         if (options.includes(choiceInLower)) {
             validatedInput = true;
             return choiceInLower;
@@ -45,11 +46,30 @@ function getPlayerChoice() {
     }
 }
 
+
 function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
     for (let i = 0; i < 5; i++) {
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+        if (checkWin(playerSelection, computerSelection) == "Player") {
+            scorePlayer++;
+        } else if (checkWin(playerSelection, computerSelection) == "Computer") {
+            scoreComputer++;
+        }
+
+    }
+
+    console.log("Game Over!")
+    if (scorePlayer > scoreComputer) {
+        console.log("Player Wins");
+    } else if (scorePlayer < scoreComputer) {
+        console.log("PC Wins")
+    } else {
+        console.log("Draw is not a Win")
     }
 }
 
