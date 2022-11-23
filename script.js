ffe004d9477a9f7b39a69e2d87fa2f5aca18b2e5
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2022-11-23 06:48:00 -0300

    added placeholder scoreboard fixed css flexbox // was really confusing fixed body padding trying to make the js logic work

--- script.js
+++ script.js
@@ -7,20 +7,106 @@ toggle.onclick = function () {
     menu.classList.toggle('active')
 }
 
-const options = ['rock', 'paper', 'scissors'];
+// Game Logic
 
-function getComputerChoice() {
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+const choices = ["rock", "paper", "scissors"];
+let winners = [];
+
+function resetGame() {
+
+
 }
 
-function checkWin(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function startGame() {
+    const rockBtn = document.querySelector('.rock')
+    const paperBtn = document.querySelector('.paper')
+    const scissorsBtn = document.querySelector('.scissors')
+
+    rockBtn.addEventListener('click', () => {
+        const computerSelection = computerPlay()
+        const playerSelection = 'rock'
+        playRound(playerSelection, computerSelection)
+    })
+
+    paperBtn.addEventListener('click', () => {
+        const computerSelection = computerPlay()
+        const playerSelection = 'paper'
+        playRound(playerSelection, computerSelection)
+    })
+
+    scissorsBtn.addEventListener('click', () => {
+        const computerSelection = computerPlay()
+        const playerSelection = 'scissors'
+        playRound(playerSelection, computerSelection)
+    })
+}
+
+function playRound(playerChoice) {
+    let wins = checkWins();
+    if (wins >= 5) {
+        return
+    }
+
+    const computerChoice = computerSelect();
+
+    const winner = checkWinner(playerChoice, computerChoice);
+
+    winner.push(winner);
+
+    tallyWins();
+    displayRound(playerChoice, computerChoice, winner);
+    wins = checkWins();
+    if (wins == 5) {
+        displayEnd()
+    }
+}
+
+function displayEnd() {
+    let playerWin = winners.filter((item) => item == "Player").length;
+
+    if (playerWin == 5) {
+        document.querySelector('.winner').textContent = "You Won"
+    } else {
+        document.querySelector('.winner').textContent = "Loser!"
+    }
+    document.querySelector('.reset').style.display = 'flex';
+
+}
+
+function displayRound(playerChoice, computerChoice, winner) {
+    document.querySelector('.playerChoice').textContent = `You Chose: ${playerChoice.charAt(0).toUppercase() + playerChoice.slice(1)}`;
+    document.querySelector('.computerChoice').textContent = `PC Chose: ${computerChoice.charAt(0).toUppercase() + computerChoice.slice(1)}`;
+    // document.querySelector('.ties').textContent = `Ties: ${ties}`;
+
+    document.querySelector('.winner').textContent = `Round Winner: ${winner}`;
+}
+
+function tallyWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    document.querySelector('.playerScore').textContent = `Score ${playerWins}`;
+    document.querySelector('.computerScore').textContent = `Score ${computerWins}`;
+    document.querySelector('.ties').textContent = `Score ${ties}`;
+}
+
+function computerSelect() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function checkWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    return Math.max(playerWins, checkWins)
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
         return "Tie";
     } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
+        (choiceP === "rock" && choiceC == "scissors") ||
+        (choiceP === "paper" && choiceC == "rock") ||
+        (choiceP === "scissors" && choiceC == "paper")
     ) {
         return "Player";
     } else {
@@ -28,57 +114,10 @@ function checkWin(playerSelection, computerSelection) {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    const result = checkWin(playerSelection, computerSelection);
-    if (result == "Tie") {
-        return "Draw!"
-    } else if (result == "Player") {
-        return `You win! ${playerSelection} beats ${computerSelection}`
-    } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
-    }
-
+function setWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
 }
 
-// function getPlayerChoice() {
-//     let validatedInput = false;
-//     while (validatedInput == false) {
-//         const choice = prompt("Battle with your life! Choose:", "Rock, Paper or Scissors");
-//         if (choice == null) {
-//             continue;
-//         } const choiceInLower = choice.toLocaleLowerCase();
-
-//         if (options.includes(choiceInLower)) {
-//             validatedInput = true;
-//             return choiceInLower;
-//         }
-//     }
-// }
-
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if (checkWin(playerSelection, computerSelection) == "Player") {
-            scorePlayer++;
-        } else if (checkWin(playerSelection, computerSelection) == "Computer") {
-            scoreComputer++;
-        }
-
-    }
-
-    console.log("Game Over!")
-    if (scorePlayer > scoreComputer) {
-        console.log("Player Wins");
-    } else if (scorePlayer < scoreComputer) {
-        console.log("PC Wins")
-    } else {
-        console.log("Draw is not a Win")
-    }
-}
-
-game()
+startGame()
